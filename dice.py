6fc0d93705cde8e2d2b7060d56c9e3eedcf83661
Medium Size <adriafrance@my.unt.edu>
2021-04-28 02:33:35 -0500

    Make interactive function

--- dice.py
+++ dice.py
@@ -1,6 +1,11 @@
 #Prob(|X1-X2|<=x)
-n = 20
-x = 3
-l = [i+1 for i in range(n-x-1)]
-P = (n**2 - 2*sum(l))/n**2
-print(P) 
+def calc_prob(sides, diff):
+  n = sides
+  x = diff
+  l = [i+1 for i in range(n-x-1)]
+  P = (n**2 - 2*sum(l))/n**2
+  return P 
+#calc_prob(20, 3)
+#calc_prob(6, 2)
+print("Calculate the probability of some difference between 2 rolls of a fair die with some number of sides:")
+calc_prob(int(input("How many sides? ")), int(input("Enter difference(inclusive): ")))
